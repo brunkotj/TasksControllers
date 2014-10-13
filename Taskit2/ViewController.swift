7897402c2bf3f169307c377202a17aa1b6899ca2
Tom Brunkow <brunkotj@gmail.com>
2014-10-13 14:53:40 -0500

    Initial Commit

---
+++ Taskit2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Taskit2
+//
+//  Created by Thomas on 10/13/14.
+//  Copyright (c) 2014 Tom Brunkow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
